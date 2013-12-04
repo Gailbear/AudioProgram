13fa6b5c0038394b11c741f8bea1b809e5134f01
Gailbear <gterman@gmail.com>
2013-12-04 17:46:56 -0500

    stef and my changes - incomplete - trying to compare files by bytes for confirmation.

--- soundmatcher.py
+++ soundmatcher.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy
 import os
 
-THRESH = 40
+THRESH = 100
 diff_thresh = 20
 
 class SoundMatcher(object):
@@ -11,7 +11,12 @@ class SoundMatcher(object):
     def __init__(self, fpdb):
         self.fpdb = fpdb
 
-    def match(self, filename1, filename2):
+    def confirm_match(self, file1, file2):
+	    return
+
+    def match(self, file1, file2):
+	    filename1 = file1[0]
+	    filename2 = file2[0]
         db1 = numpy.array(self.fpdb[filename1])
         db1len = len(db1)
         db2 = numpy.array(self.fpdb[filename2])
@@ -19,19 +24,23 @@ class SoundMatcher(object):
 
         # db1 is longer
         if db1len > db2len:
+	        longer_file = file1[1]
             longer = db1
             longer_len = db1len
             longer_name = filename1
+	        shorter_file = file2[1]
             shorter = db2
             shorter_len = db2len
             shorter_name = filename2
 
         # file2 is longer, or they're equal
         else:
+	        longer_file = file2[1]
             longer = db2
             longer_len = db2len
             longer_name = filename2
-            shorter = db1
+            shorter_file = file1[1]
+	        shorter = db1
             shorter_len = db1len
             shorter_name = filename1
 
@@ -47,19 +56,27 @@ class SoundMatcher(object):
         name2 = os.path.basename(longer_name)
         # Attempt at rabin karp
         ssub = sum(shorter)
-        ss = sum(longer[:shorter_len])
+        ss = sum(longer[0:shorter_len])
+
         for index in xrange(iters):
             dist = abs(ss-ssub)
+	        if index == 0:
+		        print dist
+		        print threshold
             if dist < threshold:
-                #doesn't work
-                match = map(lambda x,y: x == y,shorter, longer[index:shorter_len+index])
-                match = filter(lambda x: x, match)
-                ratio = float(len(match))/shorter_len
-                if ratio > maxpctmatch:
-                  maxpctmatch = ratio
+		        if confirm_match(file1[1], file2[1], index):
+		            print "MATCH %s %s (index %d, dist %d, %d%%)" % (name1, name2, index, dist, ratio * 100)
+        #doesn't work
+        #match = map(lambda x,y: abs(x-y)<100,shorter, longer[index:shorter_len+index])
+		#if index == 0:
+		#	print match[0:100]
+                #match = filter(lambda x: x, match)
+                #ratio = float(len(match))/shorter_len
+                #if ratio > maxpctmatch:
+                #  maxpctmatch = ratio
                 #if ratio > .3:
-                print "MATCH %s %s (%d, %d%%)" % (name1, name2, dist, ratio * 100)
-                return
+                #	print "MATCH %s %s (index %d, dist %d, %d%%)" % (name1, name2, index, dist, ratio * 100)
+                #	return
             if dist < mindist:
               mindist = dist
             ss = ss - longer[index] + longer[shorter_len + index -1]
